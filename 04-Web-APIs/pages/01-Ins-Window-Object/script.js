2caaf46256d459534673177f67dcc9e8c8c7e5a5
diegojimenez77 <gjpcoding8652@gmail.com>
2022-11-02 11:48:56 -0600

    adding Web API first exercises

---
+++ 04-Web-APIs/pages/01-Ins-Window-Object/script.js
@@ -0,0 +1,12 @@
+//Logs window object using this
+console.log("this: ");
+console.log(this);
+
+//Logs the document object
+console.log("window.document: ");
+console.log(window.document);
+
+//logs body of document 
+console.log("document.body: ");
+console.log(document.body);
+console.log(document.head);
