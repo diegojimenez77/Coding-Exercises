41f7c6a32f9d16fcb63f84cc192b53675e14e20a
diegojimenez77 <gjpcoding8652@gmail.com>
2022-11-03 13:16:46 -0600

    adding Create Append Stu exercise

--- 04-Web-APIs/pages/06-Stu-Setting-Attributes/script.js
+++ 04-Web-APIs/pages/06-Stu-Setting-Attributes/script.js
@@ -16,7 +16,7 @@ site2El.children[0].textContent = "Site 2";
 site2El.children[1].setAttribute("href", "https://www.wallpaperflare.com/gray-yamaha-r1-sports-bike-yamaha-yzf-r125-motorcycle-brembo-wallpaper-qwke");
 site2El.children[1].children[0].setAttribute("src", "https://c4.wallpaperflare.com/wallpaper/832/213/130/yamaha-yzf-r125-yamaha-motorcycle-brembo-wallpaper-preview.jpg");
 site2El.children[1].children[0].setAttribute("alt", "yamaha R1 2012");
-site2El.children[1].children[0].setAttribute("style", "padding:10px");
+site2El.children[1].children[0].setAttribute("style", "padding:10px,");
 site3El.children[0].textContent = "Site 3";
 site3El.children[1].setAttribute("href", "https://www.wallpaperflare.com/60th-anniversary-edition-yamaha-yzf-r1-wallpaper-srmoj");
 site3El.children[1].children[0].setAttribute("src", "https://c4.wallpaperflare.com/wallpaper/219/647/631/60th-anniversary-edition-yamaha-wallpaper-preview.jpg");
